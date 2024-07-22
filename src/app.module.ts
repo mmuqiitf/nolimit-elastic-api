8be384bb7369990c5c3f9d2afc88ff8daecde1ca
M. Muqiit Faturrahman <mmuqiit.f14@gmail.com>
2024-07-22 21:26:44 +0700

    chore: Update Elasticsearch image version to 6.8.0, remove unused search module and controller, and add countEmployees endpoint to app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SearchModule } from './search/search.module';
-
 @Module({
-  imports: [SearchModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Client } from '@elastic/elasticsearch';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello Worldie!';
+  }
+
+  async countEmployees() {
+    const esclient = new Client({
+      node: 'http://elasticsearch:9200',
+    });
+    return await esclient.search({
+      index: 'employees',
+      body: {
+        query: {
+          match_all: {},
+        },
+      },
+    });
   }
 }
